db800501917bd1c4debd7ee5f6fa74d9f979457f
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-12-31 18:05:23 +1000

    Removed default sources

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from runner import Runner
 
 
 if __name__ == '__main__' :
-    runner = Runner(
-        Clock(),
-        Packages("ephemeralex", "typesharpgen", "typesharpgenlauncher")
-    )
+
+
+    runner = Runner([
+    ])
     runner.start()
     runner.loop()
